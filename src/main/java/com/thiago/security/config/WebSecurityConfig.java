0bf250ca79b65cf9ed6cbb77566f99e8909c6adb
Thiago C. dos Santos <thiagocarretero433@gmail.com>
2023-09-02 19:10:58 -0300

    Aubuild: tenticação com Banco de Dados H2

--- src/main/java/com/thiago/security/config/WebSecurityConfig.java
+++ src/main/java/com/thiago/security/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
-package com.thiago.security;
+package com.thiago.security.config;
 
+import com.thiago.security.config.SecurityDatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,6 +21,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+
+    @Autowired
+    private SecurityDatabaseService securityService;
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
 
     //protege todos os endpoints com HTTP Basic
     @Bean
@@ -35,18 +45,18 @@ public class WebSecurityConfig {
     }
 
     // Autenticação em memória
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("thiago")
-                .roles("USERS")
-                .username("admin")
-                .password("thiagoadm")
-                .roles("MANAGERS")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("thiago")
+//                .roles("USERS")
+//                .username("admin")
+//                .password("thiagoadm")
+//                .roles("MANAGERS")
+//                .build();
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
 
 }
--- src/main/java/com/thiago/security/controller/WelcomeController.java
+++ src/main/java/com/thiago/security/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.thiago.security;
+package com.thiago.security.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
